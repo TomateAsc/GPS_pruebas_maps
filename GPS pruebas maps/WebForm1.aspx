8da6faee5d3e6f9083c385bcb318422332674454
TomateAsc <74596091+TomateAsc@users.noreply.github.com>
2021-03-20 22:31:24 -0600

    1
    trabajo, aun tiene errores, se corregira en un futuro

---
+++ GPS pruebas maps/WebForm1.aspx
@@ -0,0 +1,61 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="GPS_pruebas_maps.WebForm1" %>
+<script runat="server">
+protected void Page_Load(object sender, EventArgs e)
+{
+    this.Button2.Attributes.Add("OnClick", "javascript:return fnAceptar();");
+}
+</script>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <link href="index.css" rel="stylesheet" type="text/css">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title> 
+        Cosas de prueba
+    </title>
+    <script type="text/javascript">
+        let map;
+        let lati;
+        let long;
+        function fnAceptar() {
+            lati = 34.0660545;
+            lati = parseFloat(document.getElementById("TextBox2"));
+            long = -118.2370216;
+            long = parseFloat(document.getElementById("TextBox3"));
+            map = new google.maps.Map(document.getElementById("map"), {
+                center: { lat: lati, lng: long },
+                zoom: 8,
+            });
+        }
+    </script>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <h1>Conversor de direccion a coordenadas e inverso</h1>
+            <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+            <br/>
+            <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Button" />
+            <br/>
+            <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+            <asp:TextBox ID="TextBox3" runat="server"></asp:TextBox>
+            <br/>
+            <asp:Button ID="Button3" runat="server" Text="Button" OnClick="Button3_Click" />
+        </div>
+    </form>
+
+        <br/><br/><br/><br/><br/>
+    <div class="map">
+        <h2>mapa funcional no tan funcional</h2>
+        <button class="Button2" id="Button2" runat="server" text="mostrar mapa" />
+        <div class="map" id="map" style="width: 400px; height: 350px; border: 5px solid #5E5454;">  
+        </div>  
+    
+        <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAf-OB8eta1P90esfJDtvKSc1q518-Ouo8&callback=initMap&libraries=&v=weekly">
+
+        </script>
+        </div>
+    </body>
+</html>
---
+++ GPS pruebas maps/WebForm1.aspx.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Net; //trabaja
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Threading.Tasks;
+
+using static Ejemplo1.Resultado_Geo; //importando mi clase, donde va caer el json.
+namespace GPS_pruebas_maps
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        const string apikey = "AIzaSyCEXXEz9KDIZSV_7ij6A3xF-o6vUTHr_Uo";
+        static string url_api = "https://maps.googleapis.com/maps/api/geocode/json?address=";
+        static string url_api_rev = "https://maps.googleapis.com/maps/api/geocode/json?latlng=";
+        static string url_key = "&key=";
+        string direccion = "";
+        string coordenada1 = "";
+        string coordenada2 = "";
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            direccion = TextBox1.Text;
+            //coorde = TextBox1.Text;
+            //echo "tu texto";
+            //string coorde = ""
+            
+            coordenada1 = localizacionlat(direccion);
+            //Response.Write(string.Concat("<input id='data1' value='", coordenada1, "' />"));
+            TextBox2.Text = coordenada1;
+            //return View(coordenada1);
+            coordenada2 = localizacionlon(direccion);
+            //Response.Write(string.Concat("<input id='data2' value='", coordenada2, "' />"));
+            TextBox3.Text = coordenada2;
+            //Response.Write(localizacion(coorde));
+        }
+
+
+        /*        public async Task SendMessage(string user, string message)
+       {
+           await Clients.All.SendAsync("ReceiveMessage", user, message);
+       }
+*/
+        static string localizacionlat(string dir)
+        {
+            string url = url_api + dir.Replace(" ", "+") + url_key + apikey;
+            var json = new WebClient().DownloadString(url);
+            geo_response r_json = JsonConvert.DeserializeObject<geo_response>(json);
+
+            if (r_json.status == "OK")
+            {
+                string info = r_json.results[0].formatted_address;
+                string coordenadas = r_json.results[0].geometry.location.lat;
+                return coordenadas;
+            }
+            else
+            {
+                return json;
+            }
+        }
+        static string localizacionlon(string dir)
+        {
+            string url = url_api + dir.Replace(" ", "+") + url_key + apikey;
+            var json = new WebClient().DownloadString(url);
+            geo_response r_json = JsonConvert.DeserializeObject<geo_response>(json);
+
+            if (r_json.status == "OK")
+            {
+                string info = r_json.results[0].formatted_address;
+                string coordenadas = r_json.results[0].geometry.location.lng;
+                return coordenadas;
+            }
+            else
+            {
+                return json;
+            }
+        }
+
+        static string rev_localizacion(string coor)
+        {
+            string url = url_api_rev + coor + url_key + apikey;
+            var json = new WebClient().DownloadString(url);
+            geo_response r_json = JsonConvert.DeserializeObject<geo_response>(json);
+
+            if (r_json.status == "OK")
+            {
+                string info = r_json.results[0].formatted_address;
+                //string coordenadas = r_json.results[0].geometry.location.lat + " / " + r_json.results[0].geometry.location.lng;
+                return info;
+            }
+            else
+            {
+                return json;
+            }
+        }
+
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+            coordenada1 = TextBox2.Text;
+            coordenada2 = TextBox3.Text;
+
+            string coorde = coordenada1 + ", " + coordenada2;
+
+            TextBox1.Text = rev_localizacion(coorde);
+        }
+    }
+}
